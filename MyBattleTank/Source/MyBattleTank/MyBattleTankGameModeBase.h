51d50fe281f173827f9e5863cdb34efe8c3a6a4a
子非鱼 <zfyu222@163.com>
2016-12-08 14:15:34 +0800

    创建ue4项目

---
+++ MyBattleTank/Source/MyBattleTank/MyBattleTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MyBattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYBATTLETANK_API AMyBattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
